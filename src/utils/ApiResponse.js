9cb48be9e399581b0cb322a91fbfaf16391460a2
Aryan <aryankawale163@gmail.com>
2025-12-08 21:40:24 +0530

    add register user and bug fixes

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,9 +1,11 @@
 class ApiResponse {
-    constructor(statusCode, data, message = "success"){
-    this.statusCode = statusCode
+    constructor(statusCode, data, message = "success") {
+        this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = statusCode >=400
+        this.success = statusCode >= 400
 
     }
 }
+
+export { ApiResponse }
